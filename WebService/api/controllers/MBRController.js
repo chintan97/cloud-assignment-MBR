cad3dd22404177118a4b96b75d201992cebd53b0
smit1208 <sm252977@dal.ca>
2019-06-21 17:49:50 -0300

    user added in db

--- WebService/api/controllers/MBRController.js
+++ WebService/api/controllers/MBRController.js
@@ -7,6 +7,29 @@
 
 module.exports = {
   
+    getFundData: function(req,res){
+        var email = req.param('email');
+        var address = req.param('address');
+        var name = req.param('name');
+        var phone = req.param('phone');
+        var employer = req.param('employer');
+        var password = req.param('password');
+        
+        MBR.create({
+            name:name,
+            email:email,
+            phone:phone,
+            mailing_address:address,
+            employer_name:employer,
+            password:password
+        },function(err, MBRdata){
+            if(err){
+                return res.json(err)
+            }
+            
+            return res.json(MBRdata);
+        })
 
+    }
 };
 
